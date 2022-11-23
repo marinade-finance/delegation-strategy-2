98d34ac5dca6a0ea7e23c6c339423fdd64b276b8
Jan Legner <jan.legner@gmail.com>
2022-11-23 15:10:43 +0100

    Fixed collector scripts

--- scripts/store-quick-changes.bash
+++ scripts/store-quick-changes.bash
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+set -e
+
 SCRIPT_DIR=$(dirname "$0")
 
-"$SCRIPT_DIR/store-uptimes.bash"
-"$SCRIPT_DIR/store-commissions.bash"
-"$SCRIPT_DIR/store-versions.bash"
+"$SCRIPT_DIR/store-uptimes.bash" $@
+"$SCRIPT_DIR/store-commissions.bash" $@
+"$SCRIPT_DIR/store-versions.bash" $@
--- scripts/store-uptimes.bash
+++ scripts/store-uptimes.bash
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 SCRIPT_DIR=$(dirname "$0")
 BIN_DIR="${BIN_DIR:-"$SCRIPT_DIR/../target/debug"}"
 
@@ -9,7 +11,14 @@ then
   exit 1
 fi
 
+SNAPSHOT="$1"
+if [[ -z $SNAPSHOT ]]
+then
+  echo "Usage: $0 <snapshot-file>" >&2
+  exit 1
+fi
+
 "$BIN_DIR/store" \
   --postgres-url "$POSTGRES_URL" \
-  --snapshot-file "$SCRIPT_DIR/../snapshot.yaml" \
+  --snapshot-file "$SNAPSHOT" \
   uptime
