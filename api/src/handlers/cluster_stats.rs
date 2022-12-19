9091f57c0e3d66269376d4189316dd11de2f7db2
Jan Legner <jan.legner@gmail.com>
2022-12-19 14:45:29 +0100

    Missing file

---
+++ api/src/handlers/cluster_stats.rs
@@ -0,0 +1,44 @@
+use crate::context::WrappedContext;
+use crate::metrics;
+use crate::utils::response_error;
+use log::error;
+use serde::{Deserialize, Serialize};
+use store::dto::ClusterStats;
+use warp::{http::StatusCode, reply::json, Reply};
+
+const DEFAULT_EPOCHS: usize = 15;
+
+#[derive(Serialize, Debug)]
+pub struct Response {
+    cluster_stats: ClusterStats,
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct QueryParams {
+    epochs: Option<usize>,
+}
+
+pub async fn handler(
+    query_params: QueryParams,
+    context: WrappedContext,
+) -> Result<impl Reply, warp::Rejection> {
+    metrics::REQUEST_CLUSTER_STATS.inc();
+
+    log::info!("Query cluster stats {:?}", query_params);
+
+    let cluster_stats = context
+        .read()
+        .await
+        .cache
+        .get_cluster_stats(query_params.epochs.unwrap_or(DEFAULT_EPOCHS));
+
+    Ok(match cluster_stats {
+        Some(cluster_stats) => {
+            warp::reply::with_status(json(&Response { cluster_stats }), StatusCode::OK)
+        }
+        _ => {
+            error!("No cluster stats found");
+            response_error(StatusCode::NOT_FOUND, "Failed to fetch records!".into())
+        }
+    })
+}
