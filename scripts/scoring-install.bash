37fc7cbff325795fd6b0649836af38f66fabaf62
janlegner <jan.legner@gmail.com>
2023-03-16 12:21:39 +0100

    fix

--- scripts/scoring-install.bash
+++ scripts/scoring-install.bash
@@ -5,4 +5,3 @@ R --vanilla -e 'install.packages("data.table", repos = "http://cran.us.r-project
 R --vanilla -e 'install.packages("rmarkdown", repos = "http://cran.us.r-project.org")'
 R --vanilla -e 'install.packages("treemapify", repos = "http://cran.us.r-project.org")'
 R --vanilla -e 'install.packages("gridExtra", repos = "http://cran.us.r-project.org")'
-R --vanilla -e 'install.packages("pandoc", repos = "http://cran.us.r-project.org")'
