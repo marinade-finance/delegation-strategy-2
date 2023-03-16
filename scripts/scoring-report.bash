79f69acca0f2b60539c3b13aed137fe7716a87a2
janlegner <jan.legner@gmail.com>
2023-03-16 12:20:55 +0100

    Report

--- scripts/scoring-report.bash
+++ scripts/scoring-report.bash
@@ -5,11 +5,12 @@ set -uxo
 script_dir=$(dirname "$0")
 working_directory=${SCORING_WORKING_DIRECTORY:-"$script_dir/.."}
 file_scoring_r=${SCORING_R:-"$script_dir/scoring.R"}
+file_scoring_rmd=${SCORING_RMD:-"$script_dir/scoring.Rmd"}
 
-touch "$working_directory/scores.csv"
-touch "$working_directory/stakes.csv"
+touch "$working_directory/report.html"
 
-Rscript --vanilla "$file_scoring_r" \
+# Rscript --vanilla "$file_scoring_r" ./scores.csv ./stakes.csv ./params.env ./blacklist.csv ./validators.csv ./self-stake.csv
+Rscript -e "rmarkdown::render('$file_scoring_rmd', output_file = '$(realpath "$working_directory/report.html")')" \
     "$(realpath "$working_directory/scores.csv")" \
     "$(realpath "$working_directory/stakes.csv")" \
     "$(realpath "$working_directory/params.env")" \
