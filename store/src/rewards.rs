85195481f4802234dcc143ae1d966b6a23e0f70d
chalda <chalda@marinade.finance>
2025-07-11 15:35:31 +0200

    [mev/priority fee] fixing /rewards endpoint (#180)

--- store/src/rewards.rs
+++ store/src/rewards.rs
@@ -1,32 +1,49 @@
 use rust_decimal::Decimal;
 use tokio_postgres::Client;
 
-pub async fn get_mev_rewards(psql_client: &Client, epochs: u64) -> anyhow::Result<Vec<(u64, f64)>> {
+async fn get_jito_rewards_by_table(
+    psql_client: &Client,
+    table_name: &str,
+    epochs: u64,
+) -> anyhow::Result<Vec<(u64, f64)>> {
+    let query = format!(
+        r#"
+        SELECT SUM(total_epoch_rewards) / 1e9 AS amount, epoch
+        FROM {}
+        GROUP BY epoch
+        HAVING COUNT(CASE WHEN total_epoch_rewards IS NULL THEN 1 END) < 10
+        ORDER BY epoch DESC LIMIT $1
+        "#,
+        table_name
+    );
+
     // total_epoch_rewards may be NULL as data on commission is loaded at start of epoch
     // when JITO has not run own snapshot processing that updates data about rewards
     // query ignores whole epoch if there is at least one NULL value
     let rows = psql_client
-        .query(
-            r#"
-             SELECT SUM(total_epoch_rewards) / 1e9 AS amount, epoch
-             FROM mev
-             GROUP BY epoch
-             HAVING COUNT(CASE WHEN total_epoch_rewards IS NULL THEN 1 END) < 10
-             ORDER BY epoch
-             DESC LIMIT $1"#,
-            &[&i64::try_from(epochs)?],
-        )
+        .query(&query, &[&i64::try_from(epochs)?])
         .await?;
 
     Ok(rows
         .into_iter()
         .map(|row| {
             (
-                row.get::<_, i32>("epoch").try_into().unwrap(),
+                row.get::<_, Decimal>("epoch").try_into().unwrap(),
                 row.get::<_, Decimal>("amount").try_into().unwrap(),
             )
         })
         .collect())
+}
+
+pub async fn get_mev_rewards(psql_client: &Client, epochs: u64) -> anyhow::Result<Vec<(u64, f64)>> {
+    get_jito_rewards_by_table(psql_client, "mev", epochs).await
+}
+
+pub async fn get_jito_priority_rewards(
+    psql_client: &Client,
+    epochs: u64,
+) -> anyhow::Result<Vec<(u64, f64)>> {
+    get_jito_rewards_by_table(psql_client, "jito_priority_fee", epochs).await
 }
 
 pub async fn get_estimated_inflation_rewards(
